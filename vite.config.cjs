d15bc53f3ef34cee70e223d665b4c27720368ad6
unknown <est.cmarin@uml.edu.ni>
2023-06-13 05:07:20 +0200

    Esto es el despliegue

---
+++ vite.config.cjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+const { resolve } = require('path')
+//Add env variable to check if is production or development
+
+const env = process.env;
+//Set a base route if deploying on github pages
+env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode);
+
+export default defineConfig({
+  root: "./",
+  //Add base route if deployiing on production
+  base: env.mode === "production" ? "/Evento/" : "/",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detalles: resolve(__dirname, 'detalles.html'),
+        informacion: resolve(__dirname, 'informacion.html'),
+        locations: resolve(__dirname, 'locations.html'),
+        timeline: resolve(__dirname, 'timeline.html'),
+        tickets: resolve(__dirname, 'tickets.html'),
+        perfil: resolve(__dirname, 'perfil.html'),
+      }
+    }
+  },
+  plugins: []
+});
